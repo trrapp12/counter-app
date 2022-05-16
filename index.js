8542697c6cccab0eee9279f018afa4ecfadb6cb8
Trevor Rapp <trrapp@gmail.com>
2022-05-16 15:47:28 -0600

    adds working JS to increment, decrement, update displays, reset to zero, and save

---
+++ index.js
@@ -0,0 +1,34 @@
+(() => {
+  window.addEventListener('load', () => {
+    let total = 0;
+    const displaySection = document.getElementById('current-display');
+    const savedDisplaySecion = document.getElementById('saved-display');
+
+    function updateCurrentDisplay (input) {
+      displaySection.innerText = input
+    }
+
+    function updateSavedDisplay (input) {
+      savedDisplaySecion.innerText += ` : ${input} : `
+    }
+
+    function setToZero () {
+      total = 0;
+    }
+
+    document.getElementById('add').addEventListener('click', () => {
+      total += 1;
+      updateCurrentDisplay(total);
+    })
+    document.getElementById('minus').addEventListener('click', () => {
+      total -= 1;
+      updateCurrentDisplay(total);
+    })
+    document.getElementById('save').addEventListener('click', () => {
+      updateSavedDisplay (total);
+      setToZero();
+      updateCurrentDisplay(total)
+    })
+
+  })
+})();
